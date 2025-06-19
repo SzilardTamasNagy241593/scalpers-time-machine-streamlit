1e1ed354edf83baa230868fb00d7344e2e319ad3
SzilardTamasNagy241593 <241593@buas.nl>
2025-06-19 20:03:00 +0200

    Initial commit for HuggingFace deployment

--- scalpers-time-machine/src - Copy/scalpers_time_machine/__init__.py
+++
@@ -1,9 +0,0 @@
-__version__ = "0.1.0"
-
-from .model import train_model
-
-def greet(name: str) -> str:
-    return f"Hello, {name}! Welcome to MLOps."
-
-def square(number: int) -> int:
-    return number * number
